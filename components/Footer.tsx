6b0b04922b1734b3bab05fc3c7019d50810c51e5
ferhat <ferhattahar46@gmail.com>
2024-08-19 19:40:58 +0100

    initial commit

---
+++ components/Footer.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+
+function Footer() {
+  return (
+    <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921]">
+      <Link href="https://github.com/Ferhatmedtahar">
+        <Image
+          src="./github.svg"
+          alt="logo"
+          width={47}
+          height={44}
+          className="object-contain hover:bg-blue-800 rounded-full duration-200 transition-all cursor-pointer"
+        />
+      </Link>
+
+      <Image
+        src="./logo.svg"
+        alt="logo"
+        width={47}
+        height={44}
+        className="object-contain"
+      />
+    </footer>
+  );
+}
+
+export default Footer;
